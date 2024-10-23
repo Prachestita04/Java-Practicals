7f7916caab5d3e349bd1ec9a9044e51dc6ae8dd7
Prachestita04 <prachestitamahapatra@gmail.com>
2024-10-23 22:23:55 +0530

    third commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Programming.");
+        double d = 'a';
+        System.out.println(d);
     }
 }
