34127db4931e2361f1dcb464183f24564adbb25a
Prachestita04 <prachestitamahapatra@gmail.com>
2024-10-24 13:28:19 +0530

    fifth output commit

--- src/Example1.java
+++ src/Example1.java
@@ -1,11 +1,10 @@
 //Check the output of the following program and check the error message
 public class Example1 {
     public static void main(String[] args) {
-      while(1){
+      /*while(1){
           System.out.println("hello");
       }
-
-        /*  int x = 0;
+          int x = 0;
         if(x){
             System.out.println("hello");
         }
