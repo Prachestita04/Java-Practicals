37aab70c33259c701cc8368aa94281f4c04e1488
Prachestita04 <prachestitamahapatra@gmail.com>
2024-10-24 12:56:03 +0530

    fourth error commit

---
+++ src/Example1.java
@@ -0,0 +1,18 @@
+//Check the output of the following program and check the error message
+public class Example1 {
+    public static void main(String[] args) {
+      while(1){
+          System.out.println("hello");
+      }
+
+        /*  int x = 0;
+        if(x){
+            System.out.println("hello");
+        }
+        else{
+            System.out.println("hi");
+        }*/
+
+    }
+
+}
