b13bac8402a17eeaac02c3f98bc20d61775c73df
Prachestita04 <prachestitamahapatra@gmail.com>
2024-10-28 23:22:03 +0530

    variables commit

---
+++ src/ArrayExamples4.java
@@ -0,0 +1,39 @@
+public class ArrayExamples4 {
+    String sName;
+    int roll;
+    int sClass;
+    public void methodOne(){
+        roll =10;
+        System.out.println(roll);
+    }
+    {
+        roll = 20;
+    }
+//    ArrayExamples4(String s,int r,int c){
+//        s = this.sName;
+//        r = this.roll;
+//        c = this.sClass;
+//    }
+    public static void main(String[] args) {
+//        sName = ""
+//        ArrayExamples4 s1 = new ArrayExamples4("Mamuni",1,1);
+//        ArrayExamples4 s2 = new ArrayExamples4("Sasmita",2,1);
+        ArrayExamples4 s3 = new ArrayExamples4();
+        s3.methodOne();
+    }
+
+}
+class XYZ{
+    int a = 10;
+    int b = 20;
+    public static void main(String[] args) {
+        XYZ x1 = new XYZ();
+        System.out.println(sum(x1.a,x1.b));
+
+    }
+    public static int sum(int x, int y){
+        int c;
+        c = x + y;
+        return c;
+    }
+}
